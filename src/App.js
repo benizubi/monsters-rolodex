f8dc8cb7d8ebd630bb58effb830a56c5477fb8da
benizubi <benizubi@outlook.com>
2021-06-26 23:14:51 +0100

    using class instead of hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
+// using class instead of function to do the same thing.
+// classes offers more functionality.
 class App extends Component {
-
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>   Hello Beni    </p>
+          <button> Change Something</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
