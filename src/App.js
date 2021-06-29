f7eef0d99abb4e11451e65d16d9835459aded597
benizubi <benizubi@outlook.com>
2021-06-29 23:55:04 +0100

    Working with constructors

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Component } from 'react';
 // using class instead of function to do the same thing.
 // classes offers more functionality.
 class App extends Component {
+  constructor() {
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>   Hello Beni    </p>
-          <button> Change Somet</button>
+          <button> </button>
         </header>
       </div>
     );
