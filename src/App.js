164412bc3673544b5bdb2e3572f146352c8f6227
benizubi <benizubi@outlook.com>
2021-12-04 23:57:46 +0000

    working with classes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { render } from '@testing-library/react';
 
 class App extends Component {
   render() {
-
+    return (
+      <div className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        );
   }
 }
 
-export default App;
+        export default App;
