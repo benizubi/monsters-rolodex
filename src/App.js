945b325c2b48cc2f6950d17768bd52045faef997
benizubi <benizubi@outlook.com>
2021-12-10 23:57:05 +0000

    fixed errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     return (
       <div className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        </div>
         );
+        
   }
-  
 }
